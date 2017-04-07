2d93b06a777d35bc444819fb50b0e0428fff2919
zlevathian <hectorelopez97@hotmail.com>
2017-04-07 14:06:08 -0400

    pedo

--- test/serious-calculations-test.js
+++ test/serious-calculations-test.js
@@ -5,10 +5,10 @@ var assert = require('assert'),
 
 vows.describe('serious-calculations').addBatch({
   'When performing serious calculations': {
-    topic: seriousCalculations.performSeriousCalculations(4,3,1),
+    topic: seriousCalculations.performSeriousCalculations(4,3,2),
     'result should be valid': function (result) {
       assert.isNumber(result);
-      assert.equal(result, 7);
+      assert.equal(result, 1);
     }
   }
 }).export(module);
