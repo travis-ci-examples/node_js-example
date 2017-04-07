636f67977f4aa4431b36c8fa619507643730315b
zlevathian <hectorelopez97@hotmail.com>
2017-04-07 13:05:15 -0400

    lalalala into kakito

--- test/serious-calculations-test.js
+++ test/serious-calculations-test.js
@@ -4,10 +4,10 @@ var assert = require('assert'),
 
 vows.describe('serious-calculations').addBatch({
   'When performing serious calculations': {
-    topic: seriousCalculations.performSeriousCalculations(4),
+    topic: seriousCalculations.performSeriousCalculations(4,3),
     'result should be valid': function (result) {
       assert.isNumber(result);
-      assert.equal(result, 8);
+      assert.equal(result, 12);
     }
   }
 }).export(module);
