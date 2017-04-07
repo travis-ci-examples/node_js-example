e664ecb4682b40d314d7629f52e62cb32a789b5d
zlevathian <hectorelopez97@hotmail.com>
2017-04-07 13:10:56 -0400

    nigga aids

--- test/serious-calculations-test.js
+++ test/serious-calculations-test.js
@@ -1,6 +1,7 @@
 var assert = require('assert'),
     vows = require('vows'),
     seriousCalculations = require('../');
+    seriousCalculationss = require('../');
 
 vows.describe('serious-calculations').addBatch({
   'When performing serious calculations': {
