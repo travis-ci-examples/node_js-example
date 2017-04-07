3c9f10bfb5abf2b435c01122a97f1dc751501788
zlevathian <hectorelopez97@hotmail.com>
2017-04-07 13:55:36 -0400

    yaas

--- test/serious-calculations-test.js
+++ test/serious-calculations-test.js
@@ -5,7 +5,7 @@ var assert = require('assert'),
 
 vows.describe('serious-calculations').addBatch({
   'When performing serious calculations': {
-    topic: seriousCalculations.performSeriousCalculations(4,3,2),
+    topic: seriousCalculations.performSeriousCalculations(4,3),
     'result should be valid': function (result) {
       assert.isNumber(result);
       assert.equal(result, 7);
