f1458d2ee65d8810e83e0e99c4f2251b97a54f6e
Hlopez97 <hectorelopez97@hotmail.com>
2017-04-07 19:10:27 -0400

    changed value

--- test/serious-calculations-test.js
+++ test/serious-calculations-test.js
@@ -8,7 +8,7 @@ vows.describe('serious-calculations').addBatch({
     topic: seriousCalculations.performSeriousCalculations(6,3,4),
     'result should be valid': function calculadora (result) {
       assert.isNumber(result);
-      assert.equal(result, 2);
+      assert.equal(result, 3);
     }
   }
 }).export(module);
